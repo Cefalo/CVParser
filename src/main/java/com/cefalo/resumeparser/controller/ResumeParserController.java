170ba585f0803190d60e9364c93412818343712f
f_kabir <fasihul@cefalo.com>
2019-04-16 16:33:14 +0600

    restricted file type to be pdf

--- src/main/java/com/cefalo/resumeparser/controller/ResumeParserController.java
+++ src/main/java/com/cefalo/resumeparser/controller/ResumeParserController.java
@@ -2,7 +2,9 @@ package com.cefalo.resumeparser.controller;
 
 import com.cefalo.resumeparser.model.Resume;
 import com.cefalo.resumeparser.service.ResumeParserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +27,11 @@ public class ResumeParserController {
 
   @PostMapping(value = "/resume", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
   public ResponseEntity<Resume> parse(@RequestParam("file") MultipartFile pdf) throws IOException {
-    return ResponseEntity.ok(resumeParserService.parse(pdf.getBytes()));
+    if (StringUtils.equalsIgnoreCase(pdf.getContentType(), MediaType.APPLICATION_PDF_VALUE)) {
+      return ResponseEntity.ok(resumeParserService.parse(pdf.getBytes()));
+    }
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .build();
   }
 }
